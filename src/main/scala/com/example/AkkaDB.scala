b62fd950d20cf00380bc1f0e41cbb1be2f3938c9
arpit <arpitsingh911@gmail.com>
2019-07-25 22:19:22 +0530

    Modified receive method in AkkaDB

--- src/main/scala/com/example/AkkaDB.scala
+++ src/main/scala/com/example/AkkaDB.scala
@@ -1,7 +1,8 @@
 package com.example
 
-import akka.actor.Actor
-import com.example.Messages.{GetRequest,SetRequest,KeyNotFoundException}
+import akka.actor.{Actor, Status}
+import com.example.Messages.{GetRequest, KeyNotFoundException, SetRequest}
+
 import scala.collection.mutable
 import akka.event.Logging
 
@@ -10,9 +11,19 @@ class AkkaDB extends Actor{
   val map = new mutable.HashMap[String,Object]
   val logger = Logging(context.system,this)
 
-  override def receive: Receive = {
-    case SetRequest(key,value)=>{logger.info("received SetRequest - key: {} value: {}", key, value)
-      map.put(key, value)}
-    case o => logger.info(s"Received unknown request :{$o}")
+  override def receive = {
+    case SetRequest(key, value) =>
+      logger.info("received SetRequest - key: {} value: {}", key, value)
+      map.put(key, value)
+      sender() ! Status.Success
+    case GetRequest(key) =>
+      logger.info("received GetRequest - key: {}", key)
+      val response: Option[String] = map.get(key)
+      response match{
+        case Some(x) => sender() ! x
+        case None => sender() ! Status.Failure(new
+            KeyNotFoundException(key))
+      }
+    case o => Status.Failure(new ClassNotFoundException)
   }
 }
